de95e7399fd0f30ab90d7eb46509587473d0774f
GZYequan <1635125609@qq.com>
2018-04-01 22:45:55 +0800

    COMMIT

---
+++ src/main/java/com/yequan/o2o/service/impl/AreaServiceImpl.java
@@ -0,0 +1,19 @@
+package com.yequan.o2o.service.impl;
+
+import com.yequan.o2o.dao.AreaDao;
+import com.yequan.o2o.entity.Area;
+import com.yequan.o2o.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AreaServiceImpl implements AreaService {
+    @Autowired
+    private AreaDao areaDao;
+
+    public List<Area> queryArea() {
+        return areaDao.queryArea();
+    }
+}
---
+++ src/test/java/com/yequan/o2o/service/AreaServiceTest.java
@@ -0,0 +1,22 @@
+package com.yequan.o2o.service;
+
+import com.yequan.o2o.BaseTest;
+import com.yequan.o2o.entity.Area;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AreaServiceTest extends BaseTest {
+    @Autowired
+    private AreaService areaService;
+
+    @Test
+    public void testQueryArea(){
+        List<Area> areas = areaService.queryArea();
+        assertEquals("西苑",areas.get(0).getAreaName());
+        System.out.println(areas.get(0).getAreaName());
+    }
+}
