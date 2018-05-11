a3fc7d4ecdb39c45fd2df758096069c211f9b383
Gzyequan <673541964@qq.com>
2018-05-11 18:13:45 +0800

    redis

---
+++ src/main/java/com/yequan/o2o/exceptions/AreaOprationException.java
@@ -0,0 +1,8 @@
+package com.yequan.o2o.exceptions;
+
+public class AreaOprationException extends RuntimeException {
+
+    public AreaOprationException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/yequan/o2o/service/impl/AreaServiceImpl.java
+++ src/main/java/com/yequan/o2o/service/impl/AreaServiceImpl.java
@@ -1,19 +1,61 @@
 package com.yequan.o2o.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yequan.o2o.cache.JedisUtil;
 import com.yequan.o2o.dao.AreaDao;
 import com.yequan.o2o.entity.Area;
+import com.yequan.o2o.exceptions.AreaOprationException;
 import com.yequan.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AreaServiceImpl implements AreaService {
     @Autowired
     private AreaDao areaDao;
+    @Autowired
+    private JedisUtil.Keys jedisKeys;
+    @Autowired
+    private JedisUtil.Strings jedisStrings;
+
+    private static String AREALISTKEY = "arealist";
+
+    private static Logger logger = LoggerFactory.getLogger(AreaServiceImpl.class);
 
     public List<Area> getAreaList() {
-        return areaDao.queryArea();
+        String key = AREALISTKEY;
+        List<Area> areaList = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (!jedisKeys.exists(key)) {
+            areaList = areaDao.queryArea();
+            String jsonString = null;
+            try {
+                jsonString = objectMapper.writeValueAsString(areaList);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                logger.error(e.getMessage());
+                throw new AreaOprationException(e.getMessage());
+            }
+            jedisStrings.set(key, jsonString);
+        } else {
+            String jsonString = jedisStrings.get(key);
+            JavaType javaType = objectMapper.getTypeFactory().constructParametricType(ArrayList.class, Area.class);
+            try {
+                areaList = objectMapper.readValue(jsonString, javaType);
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error(e.getMessage());
+                throw new AreaOprationException(e.getMessage());
+            }
+        }
+        return areaList;
     }
 }
